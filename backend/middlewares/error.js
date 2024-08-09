904de7ea3caf74443d69c608cf8d3f03e09bece3
kubirthan <kubirthan7991@gmail.com>
2024-08-09 21:45:40 +0530

    Handling validations error

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -6,14 +6,24 @@ module.exports = (err, req, res, next) => {
         res.status(err.statusCode).json({
             success: false,
             message: err.message,
-            stack: err.stack
+            stack: err.stack,
+            error: err
         })
     }
 
     if(process.env.NODE_ENV == 'production') {
+        let message = err.message;
+        let error = {...err}
+       
+
+        if(err.name == "ValidationError") {
+            message = Object.values(err.errors).map(value => value.message)
+            error = new Error(message)
+        }
+
         res.status(err.statusCode).json({
             success: false,
-            message: err.message
+            message: error.message || 'Internal server error'
         })
     }
 
