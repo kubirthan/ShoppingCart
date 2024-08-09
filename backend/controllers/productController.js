2825301a5dc469853bbc35afccddab54b65eaea8
kubirthan <kubirthan7991@gmail.com>
2024-08-09 14:04:19 +0530

    Get single product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,3 +19,20 @@ exports.newProduct = async(req, res, next) => {
     product
    })
 }
+
+//Get single product
+exports.getSingleProduct = async(req, res, next) => {
+    const product = await Product.findById(req.params.id)
+
+    if(!product) {
+        res.status(404).json({
+            success: false,
+            message: "product not found"
+        })
+    }
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getproducts, newProduct } = require('../controllers/productController')
+const { getproducts, newProduct, getSingleProduct } = require('../controllers/productController')
 const router = express.Router()
 
 router.route('/products').get(getproducts)
 router.route('/product/new').post(newProduct)
+router.route('/product/:id').get(getSingleProduct)
 
 module.exports = router
